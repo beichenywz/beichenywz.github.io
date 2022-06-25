41087bdc3fa53705fd4508129567fb6623eab3d1
beichenywz <2135234195@qq.com>
2022-06-25 09:19:38 +0800

    更换inis主体

---
+++ blog/precache-manifest.c92ec9b2adfb9b99a998baeee7519af4.js
@@ -0,0 +1,646 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028d632be3a7da1f4432f49088b19c3a",
+    "url": "assets/css/app.min.css"
+  },
+  {
+    "revision": "f8b29b5b671fef3b80b515d7d4c98958",
+    "url": "assets/css/highlight/dark.min.css"
+  },
+  {
+    "revision": "3a77cf5d20e38e6744b52210cd840ea6",
+    "url": "assets/css/icons.min.css"
+  },
+  {
+    "revision": "13de54bf071fadaa4374fc7e4614866b",
+    "url": "assets/css/inis.auth.css"
+  },
+  {
+    "revision": "b1e0b68003b1dd5cd0e8f0a88e34539c",
+    "url": "assets/css/inis.media.css"
+  },
+  {
+    "revision": "c8e3ee4a15772bc16514f21725417709",
+    "url": "assets/css/inis.min.css"
+  },
+  {
+    "revision": "9ee2234ea0335f64da7c7f4ef0fbd9bd",
+    "url": "assets/css/loading.css"
+  },
+  {
+    "revision": "d4a7d0623404696ea78a7ccbd8176389",
+    "url": "assets/css/night.css"
+  },
+  {
+    "revision": "1061abebafa3babee5287acc498a02bc",
+    "url": "assets/css/root.css"
+  },
+  {
+    "revision": "72e9e39c43e5573b35c37f5f9a5e6e0c",
+    "url": "assets/css/vditor/index.css"
+  },
+  {
+    "revision": "aa7097dd10f758681f3b0463a308629b",
+    "url": "assets/fonts/Nunito-Bold.woff"
+  },
+  {
+    "revision": "0b6a4ca6f3ae3d6fa835b10169786960",
+    "url": "assets/fonts/Nunito-Regular.woff"
+  },
+  {
+    "revision": "e7ea8f78de534e08248fe3bf9689b5fa",
+    "url": "assets/fonts/Nunito-SemiBold.woff"
+  },
+  {
+    "revision": "11fc83ae11617015f2fcde2065fb34d3",
+    "url": "assets/fonts/dripicons-v2.woff"
+  },
+  {
+    "revision": "a57de8f637ace1ee9a1fd56d28c1c8ce",
+    "url": "assets/fonts/materialdesignicons-webfont.woff2"
+  },
+  {
+    "revision": "ad2ecd48a495cc67b3b33764bee90f14",
+    "url": "assets/fonts/summernote.ttf"
+  },
+  {
+    "revision": "f9b5d834b2fea5a2b440074e662c2281",
+    "url": "assets/fonts/summernote.woff"
+  },
+  {
+    "revision": "69836a7b9e86c24f93e65ec9b3866e69",
+    "url": "assets/images/bg-pattern-light.svg"
+  },
+  {
+    "revision": "ee9ca0a5ab4324951bd1c15611dee92e",
+    "url": "assets/images/bg-pattern.png"
+  },
+  {
+    "revision": "a2e130704ac30447cf7e5035e2fe0e59",
+    "url": "assets/images/empty.png"
+  },
+  {
+    "revision": "1e70af5981d267c2669ffb83a978bd14",
+    "url": "assets/images/help-icon.svg"
+  },
+  {
+    "revision": "b665f6939fa945cf17efbe77227f196f",
+    "url": "assets/images/logo.png"
+  },
+  {
+    "revision": "913f11e44dd461105a3470cb5b7fcd92",
+    "url": "assets/images/logo_night.png"
+  },
+  {
+    "revision": "d407c958ea76aad8d30d271abfcb622f",
+    "url": "assets/images/logo_sm.png"
+  },
+  {
+    "revision": "e7d604ac737347632659b65a2cf41a8d",
+    "url": "assets/images/performance.gif"
+  },
+  {
+    "revision": "ed0335d5f96b7f354bbe739e9b23b790",
+    "url": "assets/images/waves.png"
+  },
+  {
+    "revision": "9bbbbd90be37a1a414a604d57b2c07b9",
+    "url": "assets/js/404/bodymovin.js"
+  },
+  {
+    "revision": "a6919f5d3328c626b49d752d237bcaa6",
+    "url": "assets/js/404/data.js"
+  },
+  {
+    "revision": "32687f2eb340136dac11dbc6185fef98",
+    "url": "assets/js/app.min.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "assets/js/bg/0.js"
+  },
+  {
+    "revision": "1965418c6eff49b74488b458221c4544",
+    "url": "assets/js/bg/1.js"
+  },
+  {
+    "revision": "0dda8102f630eaa603974b29a3fb45d2",
+    "url": "assets/js/bg/10.js"
+  },
+  {
+    "revision": "dd26d74988d758d48f113dfd7c94e4e5",
+    "url": "assets/js/bg/11.js"
+  },
+  {
+    "revision": "7e3e81175373356f88feff081ee5d72c",
+    "url": "assets/js/bg/12.js"
+  },
+  {
+    "revision": "986237052144259f4a5d2a11a189a762",
+    "url": "assets/js/bg/13.js"
+  },
+  {
+    "revision": "362d8aaeacfbaa88f26dc8bbf827db97",
+    "url": "assets/js/bg/14.js"
+  },
+  {
+    "revision": "f2970a75f4f9b7c3c452d0b7bca5e2c3",
+    "url": "assets/js/bg/2.js"
+  },
+  {
+    "revision": "4a41de2e9d6b46e2ade3693d41ec4747",
+    "url": "assets/js/bg/3.js"
+  },
+  {
+    "revision": "8929476149659c11c556f95c3b5d03a5",
+    "url": "assets/js/bg/4.js"
+  },
+  {
+    "revision": "fa06bc5c1155337f72ecf66eb3582fa8",
+    "url": "assets/js/bg/5.js"
+  },
+  {
+    "revision": "681f0d4c781c79cea580fda7e3ad52a5",
+    "url": "assets/js/bg/6.js"
+  },
+  {
+    "revision": "a5f241e765ee309497fcc9cb881345f2",
+    "url": "assets/js/bg/7.js"
+  },
+  {
+    "revision": "3854988511f065f9a5fe9df3da628a65",
+    "url": "assets/js/bg/8.js"
+  },
+  {
+    "revision": "ef5aa63187435cbee9435b2b81f85309",
+    "url": "assets/js/bg/9.js"
+  },
+  {
+    "revision": "e7c54e135571e78684b97c56736610a6",
+    "url": "assets/json/emoji.json"
+  },
+  {
+    "revision": "f48fe9fb330a7a2f08703ee84e298101",
+    "url": "assets/libs/animate/animate.min.css"
+  },
+  {
+    "revision": "4b57cf46dc8cb95c4cca54afc85e9540",
+    "url": "assets/libs/fancybox/jquery-3.3.1.min.js"
+  },
+  {
+    "revision": "a2d42584292f64c5827e8b67b1b38726",
+    "url": "assets/libs/fancybox/jquery.fancybox.min.css"
+  },
+  {
+    "revision": "49a6b4d019a934bcf83f0c397eba82d8",
+    "url": "assets/libs/fancybox/jquery.fancybox.min.js"
+  },
+  {
+    "revision": "af097da8f73337ffc9e6a79f487953db",
+    "url": "assets/libs/lottie/index.js"
+  },
+  {
+    "revision": "25c916e69943d45330fe7ff8a8a96bbb",
+    "url": "assets/libs/lottie/json/beil.json"
+  },
+  {
+    "revision": "ffb52ecc60f46fa3778a2c7b0dfe4d0e",
+    "url": "assets/libs/lottie/json/login.json"
+  },
+  {
+    "revision": "5ce88e423e30a144d6aa99047f209da3",
+    "url": "assets/libs/lottie/json/mail.json"
+  },
+  {
+    "revision": "0972fcc5a32480b972d2c44ff0cd034c",
+    "url": "assets/libs/lottie/json/menu.json"
+  },
+  {
+    "revision": "d384da500f1b5539958f24519c46f2f4",
+    "url": "assets/libs/lottie/json/music.json"
+  },
+  {
+    "revision": "59743984997585c815ddf512914e2c7e",
+    "url": "assets/libs/lottie/json/night.json"
+  },
+  {
+    "revision": "cf7eba466ba3d255f5afe4e5638ffaa7",
+    "url": "assets/libs/lottie/json/search-sm.json"
+  },
+  {
+    "revision": "be02490c65c73ed02b647047b74d00e7",
+    "url": "assets/libs/lottie/json/search.json"
+  },
+  {
+    "revision": "6dfee623fa46c92c91ea37826555415f",
+    "url": "assets/libs/lottie/json/upup.json"
+  },
+  {
+    "revision": "92fb4091542bc68153eb6afebb703c28",
+    "url": "assets/svg/airplane.svg"
+  },
+  {
+    "revision": "d495db4fa8d98388e6800e0243e4eda1",
+    "url": "assets/svg/album.svg"
+  },
+  {
+    "revision": "eefbe0434108d1f8729f89f956c3ef78",
+    "url": "assets/svg/app.svg"
+  },
+  {
+    "revision": "3910cf8aa5a2f07a526a50fb0a943f5e",
+    "url": "assets/svg/article-comment.svg"
+  },
+  {
+    "revision": "03429f65ef9020820de33512d9594211",
+    "url": "assets/svg/backstage.svg"
+  },
+  {
+    "revision": "8791a140be04f3e9e0a19d1ffe23c0b7",
+    "url": "assets/svg/clock.svg"
+  },
+  {
+    "revision": "82d649a09d6324b4fee9fef1f8d66d52",
+    "url": "assets/svg/close.svg"
+  },
+  {
+    "revision": "6ddbec6447bf1939531ca45f914301d1",
+    "url": "assets/svg/comment.svg"
+  },
+  {
+    "revision": "1467d7c7c6ee2476642b195858dc2939",
+    "url": "assets/svg/diamond.svg"
+  },
+  {
+    "revision": "c3a2c543471df9c7869f048907a7ef67",
+    "url": "assets/svg/dom.svg"
+  },
+  {
+    "revision": "17eac21446015325ebdc8efd8a5c7e9a",
+    "url": "assets/svg/edit.svg"
+  },
+  {
+    "revision": "ef38a7a69cccc9f6b5447ac76797d568",
+    "url": "assets/svg/gitee.svg"
+  },
+  {
+    "revision": "c16dfdaec1558b244ac43acf7ce3b287",
+    "url": "assets/svg/github.svg"
+  },
+  {
+    "revision": "b472e5c70e0d17b45f5a7063cd53c5f0",
+    "url": "assets/svg/hashtag.svg"
+  },
+  {
+    "revision": "eeb3b9a014909c4ca96f0199eaf10f65",
+    "url": "assets/svg/help.svg"
+  },
+  {
+    "revision": "35387a653fe5b050400ea0eaf85bbe40",
+    "url": "assets/svg/home.svg"
+  },
+  {
+    "revision": "b4ae7b524764b8cfef8b1981d85efd60",
+    "url": "assets/svg/hot.svg"
+  },
+  {
+    "revision": "f6f66b2deaa2111bc7ae7771fe697ebb",
+    "url": "assets/svg/image.svg"
+  },
+  {
+    "revision": "4802fb958f7dd59d16f590e5fbfcb961",
+    "url": "assets/svg/like-1.svg"
+  },
+  {
+    "revision": "e80f15fc2fa70ff006612da9e29335f3",
+    "url": "assets/svg/like-2.svg"
+  },
+  {
+    "revision": "85964d52c5314be841226a9e04d94167",
+    "url": "assets/svg/like-3.svg"
+  },
+  {
+    "revision": "68bf1e932d885e975d52267e5f7ea693",
+    "url": "assets/svg/like-4.svg"
+  },
+  {
+    "revision": "72f8997ca79190e2f0bcd1989bb1050b",
+    "url": "assets/svg/like-5.svg"
+  },
+  {
+    "revision": "aa06b87cf7e86e21ff1a7b25f92d4a0f",
+    "url": "assets/svg/links.svg"
+  },
+  {
+    "revision": "fb0bb40a1d280012e6b865fa8918715d",
+    "url": "assets/svg/list.svg"
+  },
+  {
+    "revision": "b66894a612f3effe80c56f795260b840",
+    "url": "assets/svg/lock.svg"
+  },
+  {
+    "revision": "0db8e31e44257d882702bd988c7647af",
+    "url": "assets/svg/logout.svg"
+  },
+  {
+    "revision": "a2a3ac7e74b08c7b98e3eff6a1d9a411",
+    "url": "assets/svg/lower.svg"
+  },
+  {
+    "revision": "cbf4a630116a3f3bec85e8c63436f685",
+    "url": "assets/svg/manage.svg"
+  },
+  {
+    "revision": "fb7f4f47abbca471c2285167c3a47eb1",
+    "url": "assets/svg/map.svg"
+  },
+  {
+    "revision": "b8e1c09715cd8f121112d0fe29953118",
+    "url": "assets/svg/matrix.svg"
+  },
+  {
+    "revision": "0de64031d203d13d1ce50232be767546",
+    "url": "assets/svg/music-left.svg"
+  },
+  {
+    "revision": "f5e45117a690c80a14864f85c0aa18a4",
+    "url": "assets/svg/music-list.svg"
+  },
+  {
+    "revision": "b95cb88e6444fd2f6f383b8c7cd21547",
+    "url": "assets/svg/music-order.svg"
+  },
+  {
+    "revision": "e68df5d0f9bfdfa5e31f7c720777f0b0",
+    "url": "assets/svg/music-play.svg"
+  },
+  {
+    "revision": "26cd26ccb7f502876f921d2c19b828d4",
+    "url": "assets/svg/music-random.svg"
+  },
+  {
+    "revision": "6abde5613a9aff9e61ba2511ce0cd9f7",
+    "url": "assets/svg/music-right.svg"
+  },
+  {
+    "revision": "32a262630eda145535ce291d6ea77f61",
+    "url": "assets/svg/music-single.svg"
+  },
+  {
+    "revision": "ee49bf8a650fb010a45dd9431094bd27",
+    "url": "assets/svg/music-songs-1.svg"
+  },
+  {
+    "revision": "8ca111010f7e317ff40ffd976d3fefc4",
+    "url": "assets/svg/music-songs.svg"
+  },
+  {
+    "revision": "11302f9bece43e506a42019390497251",
+    "url": "assets/svg/music-sound-off.svg"
+  },
+  {
+    "revision": "3a4033d26cbdc8cb896156d3084d828d",
+    "url": "assets/svg/music-sound-on.svg"
+  },
+  {
+    "revision": "9903762bdbf5c5b0ef61ca71bce85b06",
+    "url": "assets/svg/music-stop.svg"
+  },
+  {
+    "revision": "1c737543e24b0d0f56485844f00ad4cb",
+    "url": "assets/svg/null-search.svg"
+  },
+  {
+    "revision": "7f6d7d4ec14563e8785cd769bbea78c0",
+    "url": "assets/svg/option.svg"
+  },
+  {
+    "revision": "9b45c19cfc671b9d93d76bef192254cf",
+    "url": "assets/svg/page.svg"
+  },
+  {
+    "revision": "2410dcae7f3bbe3b33a0098bd408b8cd",
+    "url": "assets/svg/plus.svg"
+  },
+  {
+    "revision": "7177dd3e1fc3ee0f6f6897bba3986e74",
+    "url": "assets/svg/position.svg"
+  },
+  {
+    "revision": "58eadc4f57f82f8691ee66cd6f45058c",
+    "url": "assets/svg/primary-left.svg"
+  },
+  {
+    "revision": "5614e519b2be4bc7bc8490e5809bfb01",
+    "url": "assets/svg/primary-lower.svg"
+  },
+  {
+    "revision": "7a019fe48cfb1260d2a3c98035b3c513",
+    "url": "assets/svg/primary-right.svg"
+  },
+  {
+    "revision": "bf9782d79818455f079d1509ece02efa",
+    "url": "assets/svg/primary-upper.svg"
+  },
+  {
+    "revision": "00400bf38d378a205637b986a1fcf8a5",
+    "url": "assets/svg/profile.svg"
+  },
+  {
+    "revision": "e074442c10de8d386b5b412b4c2f7835",
+    "url": "assets/svg/qq.svg"
+  },
+  {
+    "revision": "3530ce2f9474b66259271189e5d1f8be",
+    "url": "assets/svg/reward.svg"
+  },
+  {
+    "revision": "2803a2a464197c6a4d4f4c33230b9bf4",
+    "url": "assets/svg/right.svg"
+  },
+  {
+    "revision": "c3405ae89fa21811f063e486420753c6",
+    "url": "assets/svg/save.svg"
+  },
+  {
+    "revision": "8957d4214efbff10b4c6dfd35fd8ae8a",
+    "url": "assets/svg/self.svg"
+  },
+  {
+    "revision": "d87e76480999e3c07f04c46b03b2d4b5",
+    "url": "assets/svg/share.svg"
+  },
+  {
+    "revision": "d0b24bbcd8e712c3aa961068b19700cf",
+    "url": "assets/svg/sort.svg"
+  },
+  {
+    "revision": "728895b2a49dc08e5e2ba6aee27492a6",
+    "url": "assets/svg/table.svg"
+  },
+  {
+    "revision": "2af21e10aa565544769eec2f3865ba6d",
+    "url": "assets/svg/tag-1.svg"
+  },
+  {
+    "revision": "bd10df347cedd9689ca27472438b0684",
+    "url": "assets/svg/tag.svg"
+  },
+  {
+    "revision": "af19728b619e638915cef9f0386013e5",
+    "url": "assets/svg/tags.svg"
+  },
+  {
+    "revision": "47747992c58f6266c4565ce8d6474050",
+    "url": "assets/svg/user-1.svg"
+  },
+  {
+    "revision": "b5df88105955d38c302b890c23a96e16",
+    "url": "assets/svg/user.svg"
+  },
+  {
+    "revision": "669df4c8c0235b2d464fdf7f000595bc",
+    "url": "assets/svg/views.svg"
+  },
+  {
+    "revision": "f94b66ed54a1129afbd43b09dc8d7cdb",
+    "url": "assets/svg/wechat.svg"
+  },
+  {
+    "revision": "1b2165ab15ea03a03e6fd20e32009d1c",
+    "url": "assets/svg/weibo.svg"
+  },
+  {
+    "revision": "8ed31ebb79bc4ce8146dec66e33fdd6f",
+    "url": "assets/svg/window.svg"
+  },
+  {
+    "revision": "7bf5870155e93c73fa562c1f499e9a16",
+    "url": "assets/svg/write.svg"
+  },
+  {
+    "revision": "3a9c1fb4a0e5b632d847ad89a9c2bfd3",
+    "url": "index.html"
+  },
+  {
+    "revision": "7ed0b942cc2a6fc163517eb2cfa73dcf",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "ce2c5f07b1fa7c2f6b8ab3a2fd7022b0",
+    "url": "static/config.js"
+  },
+  {
+    "revision": "ad2ecd48a495cc67b3b33764bee90f14",
+    "url": "static/fonts/summernote.ad2ecd48.ttf"
+  },
+  {
+    "revision": "f9b5d834b2fea5a2b440074e662c2281",
+    "url": "static/fonts/summernote.f9b5d834.woff"
+  },
+  {
+    "revision": "69836a7b9e86c24f93e65ec9b3866e69",
+    "url": "static/img/bg-pattern-light.69836a7b.svg"
+  },
+  {
+    "revision": "9b2334bd567467292fc0",
+    "url": "static/js/app.69ea45ab.js"
+  },
+  {
+    "revision": "b408b9a50df1607a2188",
+    "url": "static/js/chunk-0ccabc58.f06096f4.js"
+  },
+  {
+    "revision": "afbe0a996b533344ff0e",
+    "url": "static/js/chunk-13e2c28e.9a407c8f.js"
+  },
+  {
+    "revision": "e9921364d7ac5220b709",
+    "url": "static/js/chunk-163edbeb.6193d233.js"
+  },
+  {
+    "revision": "6fc976f0c7416cd5d0a1",
+    "url": "static/js/chunk-16a6f9f5.228796a0.js"
+  },
+  {
+    "revision": "7e953deae09478222270",
+    "url": "static/js/chunk-19aa8ac4.27a59e5c.js"
+  },
+  {
+    "revision": "32bdecbb6192bc73db14",
+    "url": "static/js/chunk-286ff1fb.0df1e743.js"
+  },
+  {
+    "revision": "ae5c8015e66f42f1ec29",
+    "url": "static/js/chunk-29a521ae.ab1004f6.js"
+  },
+  {
+    "revision": "001c04b021fd250c864e",
+    "url": "static/js/chunk-3060326a.6511e7a3.js"
+  },
+  {
+    "revision": "465701f38a09c169abb4",
+    "url": "static/js/chunk-331c9343.c1342a83.js"
+  },
+  {
+    "revision": "e70dd43aa7b8436a6340",
+    "url": "static/js/chunk-428b2b06.55436981.js"
+  },
+  {
+    "revision": "073684f605cd5500144a",
+    "url": "static/js/chunk-4b844fe0.01013f5e.js"
+  },
+  {
+    "revision": "190de477c1043bdba251",
+    "url": "static/js/chunk-593753a5.90839b38.js"
+  },
+  {
+    "revision": "3a839f28ff6fe4cab170",
+    "url": "static/js/chunk-593b9d70.d8f8763c.js"
+  },
+  {
+    "revision": "6614c0f505515a01e15b",
+    "url": "static/js/chunk-5ad5e61e.81109640.js"
+  },
+  {
+    "revision": "6610033f31cd76b02999",
+    "url": "static/js/chunk-6b4219ed.95f477b7.js"
+  },
+  {
+    "revision": "c2a2780141a78a7b08bc",
+    "url": "static/js/chunk-70f294f3.10dc00c8.js"
+  },
+  {
+    "revision": "b873fbfebe962b40f5c8",
+    "url": "static/js/chunk-746cb4a2.f987f4a6.js"
+  },
+  {
+    "revision": "435b74121dcf198076e6",
+    "url": "static/js/chunk-7594b915.e8570e96.js"
+  },
+  {
+    "revision": "e4582720635a73d159bd",
+    "url": "static/js/chunk-79f3a313.8ea906c5.js"
+  },
+  {
+    "revision": "19ecf397363acb765554",
+    "url": "static/js/chunk-8e0a295a.9a007270.js"
+  },
+  {
+    "revision": "5f7eb1c9c6a3c7caaa0a",
+    "url": "static/js/chunk-b135a8da.ba7f2c73.js"
+  },
+  {
+    "revision": "9f90d807a575796bbd6c",
+    "url": "static/js/chunk-b4dfa8fa.ec09e1f3.js"
+  },
+  {
+    "revision": "af1e4409e9971a283376",
+    "url": "static/js/chunk-vendors.86092269.js"
+  }
+]);
