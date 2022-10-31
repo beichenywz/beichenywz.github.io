54f341390dab71210de4b8da58c6ade4b1924949
beichenywz <2135234195@qq.com>
2022-11-01 05:13:59 +0800

    ppt

--- assets/cloud/js/JavaScript .js
+++ assets/cloud/js/JavaScript .js
@@ -1,15 +1,3 @@
 function GoToUrl1() {
     window.open(document.getElementById("form_select1").value, "target", "");
 }
-
-function GoToUrl2() {
-    window.open(document.getElementById("form_select2").value, "target", "");
-}
-
-function GoToUrl3() {
-    window.open(document.getElementById("form_select3").value, "target", "");
-}
-
-function GoToUrl4() {
-    window.open(document.getElementById("form_select4").value, "target", "");
-}
